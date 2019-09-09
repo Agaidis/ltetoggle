92a0c41876501440a712f1d64cf6d32e8c972c4b
Andrew Gaidis <andrewgaidis@Andrews-MacBook-Pro.local>
2019-09-08 20:59:50 -0400

    changing name to Toggle

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>{{ config('app.name', 'Toggle') }}</title>
     <link rel="icon" href="https://quickevict.nyc3.digitaloceanspaces.com/lexathonenergy.jpg"/>
 
 
