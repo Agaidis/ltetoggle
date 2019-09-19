9297b277aa92924ad67e53609d11f6eedcbc355a
Andrew Gaidis <andrewgaidis@Andrews-MacBook-Pro.local>
2019-09-18 21:51:16 -0400

    trying out data

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -28,9 +28,9 @@
                             <table class="table table-hover table-responsive-md table-bordered" id="table_one">
                                 <thead>
                                 <tr>
-                                    <th class="text-center">Name</th>
+                                    <th class="text-center">Lease Id</th>
                                     <th class="text-center">Acreage</th>
-                                    <th class="text-center">Operator on Lease</th>
+                                    <th class="text-center">State</th>
                                     <th class="text-center">Block 4 Section</th>
                                     <th class="text-center">6IS (TRAC)</th>
                                     <th class="text-center">Action</th>
@@ -38,127 +38,17 @@
                                 </tr>
                                 </thead>
                                 <tbody>
+                                @foreach ($data as $lease)
                                 <tr>
-                                    <td class="text-center">Andrew Gaidis</td>
-                                    <td class="text-center">40</td>
-                                    <td class="text-center">Todd Packer</td>
-                                    <td class="text-center">Zilker</td>
-                                    <td class="text-center"><a href="http://wwwgisp.rrc.texas.gov/GISViewer2" target="_blank">6IS (TRAC)</a></td>
-                                    <td class="text-center">
+                                    <td class="text-center">{{$lease->LeaseId}}</td>
+                                    <td class="text-center">{{$lease->AcreaAcres}}</td>
+                                    <td class="text-center">{{$lease->State}}</td>
                                         <button type="button" data-target="#modal_edit_magistrate" data-toggle="modal" class="pdf_download_btn_dashboard btn-sm btn-primary fas fa-edit"></button>
                                         <button type="button" class="fa fa-trash btn-sm btn-danger"></button>
                                     </td>
                                 </tr>
-                                <tr>
-                                    <td class="text-center">Matt Foley</td>
-                                    <td class="text-center">200</td>
-                                    <td class="text-center">Adam Cook</td>
-                                    <td class="text-center">South Congress</td>
-                                    <td class="text-center"><a href="http://wwwgisp.rrc.texas.gov/GISViewer2" target="_blank">6IS (TRAC)</a></td>
-                                    <td class="text-center">
-                                        <button type="button" data-target="#modal_edit_magistrate" data-toggle="modal" class="pdf_download_btn_dashboard btn-sm btn-primary fas fa-edit"></button>
-                                        <button type="button" class="fa fa-trash btn-sm btn-danger"></button>
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td class="text-center">Michael Scott</td>
-                                    <td class="text-center">186</td>
-                                    <td class="text-center">Brian Cook</td>
-                                    <td class="text-center">Archer</td>
-                                    <td class="text-center"><a href="http://wwwgisp.rrc.texas.gov/GISViewer2" target="_blank">6IS (TRAC)</a></td>
-                                    <td class="text-center">
-                                        <button type="button" data-target="#modal_edit_magistrate" data-toggle="modal" class="pdf_download_btn_dashboard btn-sm btn-primary fas fa-edit"></button>
-                                        <button type="button" class="fa fa-trash btn-sm btn-danger"></button>
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td class="text-center">Robert Troyman</td>
-                                    <td class="text-center">268</td>
-                                    <td class="text-center">Joe Daze</td>
-                                    <td class="text-center">Throckmorton</td>
-                                    <td class="text-center"><a href="http://wwwgisp.rrc.texas.gov/GISViewer2" target="_blank">6IS (TRAC)</a></td>
-                                    <td class="text-center">
-                                        <button type="button" data-target="#modal_edit_magistrate" data-toggle="modal" class="pdf_download_btn_dashboard btn-sm btn-primary fas fa-edit"></button>
-                                        <button type="button" class="fa fa-trash btn-sm btn-danger"></button>
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td class="text-center">John Adams</td>
-                                    <td class="text-center">286</td>
-                                    <td class="text-center">Dean Perkins</td>
-                                    <td class="text-center">Runnells</td>
-                                    <td class="text-center"><a href="http://wwwgisp.rrc.texas.gov/GISViewer2" target="_blank">6IS (TRAC)</a></td>
-                                    <td class="text-center">
-                                        <button type="button" data-target="#modal_edit_magistrate" data-toggle="modal" class="pdf_download_btn_dashboard btn-sm btn-primary fas fa-edit"></button>
-                                        <button type="button" class="fa fa-trash btn-sm btn-danger"></button>
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td class="text-center">James Johnson</td>
-                                    <td class="text-center">89</td>
-                                    <td class="text-center">Adam Cook</td>
-                                    <td class="text-center">Mitchell</td>
-                                    <td class="text-center"><a href="http://wwwgisp.rrc.texas.gov/GISViewer2" target="_blank">6IS (TRAC)</a></td>
-                                    <td class="text-center">
-                                        <button type="button" data-target="#modal_edit_magistrate" data-toggle="modal" class="pdf_download_btn_dashboard btn-sm btn-primary fas fa-edit"></button>
-                                        <button type="button" class="fa fa-trash btn-sm btn-danger"></button>
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td class="text-center">Howard Reynolds</td>
-                                    <td class="text-center">48</td>
-                                    <td class="text-center">Craig Lozada</td>
-                                    <td class="text-center">Howard</td>
-                                    <td class="text-center"><a href="http://wwwgisp.rrc.texas.gov/GISViewer2" target="_blank">6IS (TRAC)</a></td>
-                                    <td class="text-center">
-                                        <button type="button" data-target="#modal_edit_magistrate" data-toggle="modal" class="pdf_download_btn_dashboard btn-sm btn-primary fas fa-edit"></button>
-                                        <button type="button" class="fa fa-trash btn-sm btn-danger"></button>
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td class="text-center">John Smith</td>
-                                    <td class="text-center">16</td>
-                                    <td class="text-center">Chris Snyder</td>
-                                    <td class="text-center">Concho</td>
-                                    <td class="text-center"><a href="http://wwwgisp.rrc.texas.gov/GISViewer2" target="_blank">6IS (TRAC)</a></td>
-                                    <td class="text-center">
-                                        <button type="button" data-target="#modal_edit_magistrate" data-toggle="modal" class="pdf_download_btn_dashboard btn-sm btn-primary fas fa-edit"></button>
-                                        <button type="button" class="fa fa-trash btn-sm btn-danger"></button>
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td class="text-center">John Moody</td>
-                                    <td class="text-center">25</td>
-                                    <td class="text-center">Charles Andrews</td>
-                                    <td class="text-center">Menard</td>
-                                    <td class="text-center"><a href="http://wwwgisp.rrc.texas.gov/GISViewer2" target="_blank">6IS (TRAC)</a></td>
-                                    <td class="text-center">
-                                        <button type="button" data-target="#modal_edit_magistrate" data-toggle="modal" class="pdf_download_btn_dashboard btn-sm btn-primary fas fa-edit"></button>
-                                        <button type="button" class="fa fa-trash btn-sm btn-danger"></button>
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td class="text-center">William Sullivan</td>
-                                    <td class="text-center">744</td>
-                                    <td class="text-center">Alan Nagel</td>
-                                    <td class="text-center">Sterling</td>
-                                    <td class="text-center"><a href="http://wwwgisp.rrc.texas.gov/GISViewer2" target="_blank">6IS (TRAC)</a></td>
-                                    <td class="text-center">
-                                        <button type="button" data-target="#modal_edit_magistrate" data-toggle="modal" class="pdf_download_btn_dashboard btn-sm btn-primary fas fa-edit"></button>
-                                        <button type="button" class="fa fa-trash btn-sm btn-danger"></button>
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td class="text-center">Jefferey Bradshaw</td>
-                                    <td class="text-center">900</td>
-                                    <td class="text-center">Kevin Smith</td>
-                                    <td class="text-center">Upton</td>
-                                    <td class="text-center"><a href="http://wwwgisp.rrc.texas.gov/GISViewer2" target="_blank">6IS (TRAC)</a></td>
-                                    <td class="text-center">
-                                        <button type="button" data-target="#modal_edit_magistrate" data-toggle="modal" class="pdf_download_btn_dashboard btn-sm btn-primary fas fa-edit"></button>
-                                        <button type="button" class="fa fa-trash btn-sm btn-danger"></button>
-                                    </td>
-                                </tr>
+
+                                @endforeach
                                 </tbody>
                                 <tfoot>
                                 <caption id="lease_table_caption">Leases: Non-Producing & Producing</caption>
