5b4919a4e6a27687e5392716e11cd8d897208918
Andrew Gaidis <andrewgaidis@Andrews-MacBook-Pro.local>
2019-09-19 11:42:45 -0400

    trying out data

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -23,9 +23,6 @@
                         </div>
                         <div class="row justify-content-center">
                             <div class="col-md-10">
-                                @foreach ($Leases as $key => $value)
-                                    <span>{{$value}}</span>
-                                @endforeach
                             <table class="table table-hover table-responsive-md table-bordered" id="table_one">
                                 <thead>
                                 <tr>
