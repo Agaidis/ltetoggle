9708cf55bcaa157f8f3f643adbf9cbc3b9842e40
Andrew Gaidis <andrewgaidis@Andrews-MacBook-Pro.local>
2019-09-18 21:59:09 -0400

    trying out data

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -24,7 +24,7 @@
                         <div class="row justify-content-center">
                             <div class="col-md-10">
 
-                                {{$data[0]}}
+                                {{$data}}
 
                             <table class="table table-hover table-responsive-md table-bordered" id="table_one">
                                 <thead>
