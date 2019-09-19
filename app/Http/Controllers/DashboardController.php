7b260d955bfd6503bab3c73268d89dc8e563fbc3
Andrew Gaidis <andrewgaidis@Andrews-MacBook-Pro.local>
2019-09-18 21:12:54 -0400

    trying out data

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -35,7 +35,7 @@ class DashboardController extends Controller
         try {
             $response = $client->request('GET', 'https://di-api.drillinginfo.com/v2/direct-access/landtrac-leases', [
 
-                'Authorization' => ['Token' => 'Bearer ' . $this->apiToken],
+                'Authorization' => ['Bearer Token' => $this->apiToken],
                 'headers' => [
                     'X-API-KEY' => $this->apiKey,
                     'Authorization' => 'Bearer '.$this->apiToken,
