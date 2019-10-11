e53e876a703ac89cf66a9c867c4129ea1b219360
Andrew Gaidis <andrewgaidis@Andrews-MacBook-Pro.local>
2019-10-11 14:46:55 -0400

    adding queue for permits

--- resources/views/newPermits.blade.php
+++ resources/views/newPermits.blade.php
@@ -41,7 +41,7 @@
                                                         @endforeach
                                                     </select>
                                                 </td>
-                                                <td class="text-center">{{$approvedDate[0]}}</td>
+                                                <td class="text-center">{{$permit->ApprovedDate}}</td>
                                                 <td class="text-center">{{$permit->drill_type}}</td>
                                                 <td class="text-center">
                                                     <button type="button" data-target="#modal_show_permit" data-toggle="modal" id="id_{{$permit->permit_id}}" class="fa fa-edit btn-sm btn-primary view_permit"></button>
