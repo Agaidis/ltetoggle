51f11f79413b1fa0bf96ec0a1e2454ec2e25586a
Andrew Gaidis <andrewgaidis@Andrews-MacBook-Pro.local>
2019-12-16 12:39:29 -0500

    committing owners

--- app/Http/Controllers/MineralOwnersController.php
+++ app/Http/Controllers/MineralOwnersController.php
@@ -10,7 +10,11 @@ class MineralOwnersController extends Controller
     public function index(Request $request) {
         $leaseNames = array();
 
-        $owners = MineralOwner::where('owner', $request->operator)->distinct()->get();
+        $owners = MineralOwner::where('operator_company_name', $request->operator)->distinct()->get();
+
+        if ($owners->isEmpty()) {
+            $owners = MineralOwner::where('owner', $request->operator)->distinct()->get();
+        }
 
        foreach ($owners as $owner) {
            array_push($leaseNames, $owner->lease_name);
