99234fbf5b631241c4584dc3bec41b6dfbeac595
Andrew Gaidis <andrewgaidis@Andrews-MBP-2.hsd1.md.comcast.net>
2020-01-12 13:55:04 -0500

    committing progress

--- resources/views/owner.blade.php
+++ resources/views/owner.blade.php
@@ -8,6 +8,48 @@
                     <div class="card-header">Owners Page</div>
                     <div class="card-body">
                         <h2 style="text-align:center;">Owner Name: {{$ownerName}}</h2>
+
+                        <div class="row">
+                             <div class="col-offset-2 col-md-4">
+                                 <div>
+                                     <table class="table table-hover table-responsive-md table-bordered" id="owner_table">
+                                         <thead>
+                                         <tr>
+                                             <th class="text-center">Phone Description</th>
+                                             <th class="text-center">Phone Number</th>
+                                         </tr>
+                                         </thead>
+                                         <tbody>
+                                         @foreach ($ownerPhoneNumbers as $ownerPhoneNumber)
+                                             <tr>
+                                                 <td class="text-center">{{$ownerPhoneNumber->phone_desc}}</td>
+                                                 <td class="text-center">{{$ownerPhoneNumber->phone_number}}</td>
+                                             </tr>
+                                         @endforeach
+
+                                         </tbody>
+                                     </table>
+                                 </div>
+                             </div>
+                            <div class="col-offset-2 col-md-4">
+                                <div>
+                                    <table class="table table-hover table-responsive-md table-bordered" id="owner_table">
+                                        <thead>
+                                        <tr>
+                                            <th class="text-center">Notes</th>
+                                        </tr>
+                                        </thead>
+                                        <tbody>
+                                        @foreach ($ownerNotes as $ownerNote)
+                                            <tr>
+                                                <td class="text-center">{!! $ownerNote->notes !!}</td>
+                                            </tr>
+                                        @endforeach
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
