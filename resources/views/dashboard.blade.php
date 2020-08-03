b5c512cb2fe1565ba32df7d41fc84781005a207d
Andrew Gaidis <andrewgaidis@Andrews-MacBook-Pro-2.local>
2020-08-02 20:20:37 -0400

    committing progress

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -391,7 +391,7 @@
                                                                     <option selected value="purple">Completed</option>
                                                                 </select>
                                                             @else
-                                                                <select id="toggle_status_{{$nvxPermit->permit_id}}" class="form-control toggle_status yellow">
+                                                                <select id="toggle_status_{{$npp->permit_id}}" class="form-control toggle_status yellow">
                                                                     <option selected value="yellow">Untouched </option>
                                                                     <option value="green">Major Prospect </option>
                                                                     <option value="blue">Quality Prospect </option>
