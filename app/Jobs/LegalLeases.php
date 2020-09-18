904693389ed5f930024dabaee88644b2afd813bf
Andrew Gaidis <andrewgaidis@Andrews-MacBook-Pro-2.local>
2020-09-17 22:30:08 -0400

    committing horizon and upgrades

--- app/Jobs/LegalLeases.php
+++ app/Jobs/LegalLeases.php
@@ -2,6 +2,7 @@
 
 namespace App\Jobs;
 
+use App\LegalLease;
 use Illuminate\Bus\Queueable;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Queue\InteractsWithQueue;
@@ -13,14 +14,16 @@ class LegalLeases implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
+    protected $leases;
     /**
      * Create a new job instance.
      *
+     * @var $leases
      * @return void
      */
-    public function __construct()
+    public function __construct($leases)
     {
-
+        $this->leases = $leases;
     }
 
     /**
@@ -30,6 +33,37 @@ class LegalLeases implements ShouldQueue
      */
     public function handle()
     {
-        Log::info('yay');
+        if ($this->leases[0] != null && $this->leases[0] != '' && isset($this->leases[0])) {
+
+            $decodedLeases = json_decode($this->leases[0]);
+
+            for ($i = 0; $i < count($decodedLeases); $i++) {
+
+                LegalLease::updateOrCreate(['LeaseId' => $decodedLeases[$i]->LeaseId],
+                    [
+                        'MappingID' => $decodedLeases[$i]->MappingID,
+                        'AreaAcres' => $decodedLeases[$i]->AreaAcres,
+                        'Abstract' => $decodedLeases[$i]->Abstract,
+                        'AbstractNo' => $decodedLeases[$i]->AbstractNo,
+                        'Block' => $decodedLeases[$i]->Block,
+                        'CountyParish' => $decodedLeases[$i]->CountyParish,
+                        'Created' => $decodedLeases[$i]->Created,
+                        'Geometry' => $decodedLeases[$i]->Geometry,
+                        'LatitudeWGS84' => $decodedLeases[$i]->LatitudeWGS84,
+                        'LongitudeWGS84' => $decodedLeases[$i]->LongitudeWGS84,
+                        'Grantee' => $decodedLeases[$i]->Grantee,
+                        'GranteeAddress' => $decodedLeases[$i]->GranteeAddress,
+                        'GranteeAlias' => $decodedLeases[$i]->GranteeAlias,
+                        'Grantor' => $decodedLeases[$i]->Grantor,
+                        'GrantorAddress' => $decodedLeases[$i]->GrantorAddress,
+                        'MaxDepth' => $decodedLeases[$i]->MaxDepth,
+                        'MinDepth' => $decodedLeases[$i]->MinDepth,
+                        'Range' => $decodedLeases[$i]->Range,
+                        'Section' => $decodedLeases[$i]->Section,
+                        'Township' => $decodedLeases[$i]->Township,
+                        'RecordDate' => $decodedLeases[$i]->RecordDate
+                    ]);
+            }
+        }
     }
 }
